b81edea494b2ba70ecffbf4eb70ff4b3050871cf
Lev Hnitsa <lev.hnitsa@amomedia.com>
2022-06-30 17:25:40 +0300

    add king logic

--- src/models/figures/King.tsx
+++ src/models/figures/King.tsx
@@ -2,7 +2,7 @@ import kingBlack from '../../assets/king-black.png';
 import kingWhite from '../../assets/king-white.png';
 import { BoardModel } from '../BoardModel';
 import { Figure } from './Figure';
-import { Names, Side } from './types/common';
+import { Coords, Names, Side } from './types/common';
 
 export class King extends Figure {
   side;
@@ -22,5 +22,62 @@ export class King extends Figure {
 
     this.side = side;
     this.board = board;
+  }
+
+  private setCells({ x, y }: Coords) {
+    if (y - 1 >= this.minCoord && x - 1 >= this.minCoord) {
+      this.checkNextCell(
+        this.board.cells[y - 1][x - 1],
+        this.side,
+      );
+    }
+    if (y - 1 >= this.minCoord && x + 1 <= this.maxCoord) {
+      this.checkNextCell(
+        this.board.cells[y - 1][x + 1],
+        this.side,
+      );
+    }
+    if (y + 1 <= this.maxCoord && x + 1 <= this.maxCoord) {
+      this.checkNextCell(
+        this.board.cells[y + 1][x + 1],
+        this.side,
+      );
+    }
+    if (y + 1 <= this.maxCoord && x - 1 >= this.minCoord) {
+      this.checkNextCell(
+        this.board.cells[y + 1][x - 1],
+        this.side,
+      );
+    }
+    if (y - 1 >= this.minCoord) {
+      this.checkNextCell(
+        this.board.cells[y - 1][x],
+        this.side,
+      );
+    }
+    if (y + 1 <= this.maxCoord) {
+      this.checkNextCell(
+        this.board.cells[y + 1][x],
+        this.side,
+      );
+    }
+    if (x - 1 >= this.minCoord) {
+      this.checkNextCell(
+        this.board.cells[y][x - 1],
+        this.side,
+      );
+    }
+    if (x + 1 <= this.maxCoord) {
+      this.checkNextCell(
+        this.board.cells[y][x + 1],
+        this.side,
+      );
+    }
+  }
+
+  public getAvailableCoords(coords: Coords) {
+    this.setCells(coords);
+
+    this.board.refreshCells();
   }
 }
